6b2db0c6c856eceb4e997db9c190e214ac884134
allanescher <allanbescher@hotmail.com>
2022-04-04 01:51:35 -0300

    ajustes conforme últimos ajustes realizados em aula

--- disc/urls.py
+++ disc/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from disc_website import views
@@ -21,4 +22,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('pergunta_form/', views.pergunta_form, name='pergunta_form'),
+    path("teste/<int:teste>/", views.teste, name="teste"),
 ]
--- disc_website/admin.py
+++ disc_website/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from disc_website.models import Alternativa, Aluno, Resultado, Teste, Turma
+from disc_website.models import Alternativa, Aluno, Resultado, Teste, Turma, Pergunta
 
-from disc_website.models import Pergunta
 # Register your models here.
+
+class ResultadoAdmin(admin.ModelAdmin):
+    list_display = ('aluno', 'dominancia', 'influencia' ,'cautela', 'estabilidade')
+    search_fielda = ('aluno__ra__icontains',)
+
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('ra', 'nome', 'email')
+    search_fields = ('ra_icontains', 'nome_icontains')
 
 admin.site.register(Pergunta)
 admin.site.register(Alternativa)
 admin.site.register(Teste)
-admin.site.register(Resultado)
-admin.site.register(Aluno)
+admin.site.register(Resultado, ResultadoAdmin)
+admin.site.register(Aluno, AlunoAdmin)
 admin.site.register(Turma)
